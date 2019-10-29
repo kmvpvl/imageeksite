366d1886ca2635d5d43562d660857eab13d670e2
Pavel Komov <kmvpvl@ya.ru>
2019-10-29 23:23:55 +0300

    pwd for prod

--- dbconnr.php
+++ dbconnr.php
@@ -1,6 +1,6 @@
 <?php
 $dbname = 'imageek';
-$conn = mysql_connect('localhost','imageeku','jyoncSNOw8oEKCLn');
+$conn = mysql_connect('localhost','imageeku','2kq2fOKx');
 if (!$conn) die('DB Server is not available: '. mysql_error());
 $db = mysql_select_db($dbname, $conn);
 if (!$db) die('DB sh is not available: ' . mysql_error());
