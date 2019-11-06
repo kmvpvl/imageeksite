6e3242ff6e44109734d9e3f09f046df0ed8f0977
Pavel Komov <kmvpvl@gmail.com>
2019-11-06 20:25:07 +0300

    google search

--- header.php
+++ header.php
@@ -1,4 +1,14 @@
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151696798-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-151696798-1');
+</script>
+
 <title>I'm a geek %)</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8">
