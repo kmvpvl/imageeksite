6dc4e33715486be73284fe8ec19c45d37686078a
Pavel Komov <kmvpvl@gmail.com>
2019-11-30 11:30:06 +0300

    test for disk free space1

--- phpinfot.php
+++ phpinfot.php
@@ -1,6 +1,6 @@
 <?php
     echo "Сегодня - ".date("d F Y")."<br>";
     echo "Текущее время - ".date("H:i:s")."<br>";
-    echo "Место на диске - " . disk_free_space(".")/1024/1024 . "<br>";
+    echo "Место на диске - " . disk_free_space("../../../")/1024/1024 . "<br>";
     echo "Текущий каталог - " . getcwd() . "<br>";
 ?>
